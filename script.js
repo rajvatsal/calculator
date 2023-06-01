08fd903298db72cb6a3fa277fe26cd0e91672918
Vatsal <rajvatsal2004@gmail.com>
2023-06-01 21:36:47 +0530

    feat: Call function operate on pressing "="

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function operate(n1, n2, op){
+function operate(n1, op, n2){
+    n1 = Math.floor(parseFloat(n1) * 100) / 100;
+    n2 = Math.floor(parseFloat(n2) * 100) / 100;
     this.calc = {
-        "+":  (a, b) => a+b,
-        "-":  (a, b) => a-b,
-        "*":  (a, b) => a*b,
-        "/":  (a, b) => a/b,
-        "**": (a, b) => a**b,
-        "%":  (a, b) => a%b,
+        "+":  (a, b) => a + b,
+        "-":  (a, b) => a - b,
+        "*":  (a, b) => a * b,
+        "/":  (a, b) => a / b,
+        "**": (a, b) => a ** b,
+        "%":  (a, b) => a % b,
     }
-    return this.calc[op](n1, n2)
+    screen.textContent = this.calc[op](n1, n2)
 }
 function printScreen(e){
     screen.textContent += e.target.textContent;
@@ -17,3 +19,7 @@ const buttons = document.querySelectorAll(".screen-text");
 const screen = document.querySelector(".screen");
 buttons.forEach(button => button.addEventListener("mousedown", printScreen));
 document.querySelector(".clear").addEventListener("mousedown", () => screen.textContent = "")
+document.querySelector(".equals-to").addEventListener("mousedown", () => {
+    let calculateString = screen.textContent.split(" ");
+    operate(...calculateString);
+});
