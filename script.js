df3531716e353e682e7c94b39f79de19aaeeb38f
Vatsal <rajvatsal2004@gmail.com>
2023-06-01 20:39:45 +0530

    feature: Add function printScreen

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ function operate(n1, n2, op){
     }
     return this.calc[op](n1, n2)
 }
+function printScreen(e){
+    screen.textContent = e.target.textContent;
+}
+
+const buttons = document.querySelectorAll("button");
+const screen = document.querySelector(".screen");
+buttons.forEach(button => button.addEventListener("mousedown", printScreen));
