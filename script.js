0d168813b18b03fe5cd074b653d9384a7208e868
Vatsal <rajvatsal2004@gmail.com>
2023-06-01 20:56:56 +0530

    feat: Add class screen-text to buttons
    except to clear and = button

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function printScreen(e){
     screen.textContent += e.target.textContent;
 }
 
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".screen-text");
 const screen = document.querySelector(".screen");
 buttons.forEach(button => button.addEventListener("mousedown", printScreen));
 document.querySelector(".clear").addEventListener("mousedown", () => screen.textContent = "")
