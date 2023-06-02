7ea8ed642d03d772389b3a53e2396007628b4aa4
Vatsal <rajvatsal2004@gmail.com>
2023-06-02 11:47:32 +0530

    feat: Add function getRickRolled on n2 === 0

--- script.js
+++ script.js
@@ -6,41 +6,60 @@ function operate(n1, op, n2){
         "+":  (a, b) => a + b,
         "-":  (a, b) => a - b,
         "*":  (a, b) => Math.floor((a * b) * 100) / 100,
-        "/":  (a, b) => Math.floor((a / b) * 100) / 100,
+        "/":  (a, b) => {
+            if(b === 0) getRickRolled();
+            Math.floor((a / b) * 100) / 100;
+        },
         "**": (a, b) => a ** b,
         "%":  (a, b) => a % b,
     }
     screen.textContent = this.calc[op](n1, n2)
 }
 function printScreen(e){
-    if(e.target.textContent === ".") e.target.removeEventListener("mousedown", printScreen);
+    if(e.target.textContent === ".") e.target.removeEventListener('mousedown', printScreen);
     screen.textContent += e.target.textContent;
 }
 function operateFirstTwoNum(e){
     operatorCount++;
-    dotButton.addEventListener("mousedown", printScreen);
-    
-    if(operatorCount !== 2) return;
+    dotButton.addEventListener('mousedown', printScreen);
+
+    if(operatorCount !== 2) return
     let calculateString = screen.textContent.split(" ");
     operate(...calculateString);
+    if(screen.textContent === "") {
+        resetValues();
+        return
+    }
     screen.textContent += e.target.textContent;
     operatorCount--;
 }
 function resetValues(){
     operatorCount = 0;
-    dotButton.addEventListener("mousedown", printScreen);
+    dotButton.addEventListener('mousedown', printScreen);
     screen.textContent = "";
 }
+function getRickRolled(){
+    let vid = document.createElement('video');
+    let src = document.createElement('source');
+    let main = document.querySelector('main');
+    vid.setAttribute('height', '100%');
+    vid.setAttribute('width', '100%');
+    src.setAttribute('src', './pics-vids/get-rick-rolled.mp4');
+    vid.appendChild(src);
+    main.insertBefore(vid, screen);
+    vid.play();
+    vid.addEventListener('ended', () => main.removeChild(vid));
+}
 
-const buttons = document.querySelectorAll(".screen-text");
-const screen = document.querySelector(".screen");
-const operators = document.querySelectorAll(".operator");
-const dotButton = document.querySelector("#dot");
+const buttons = document.querySelectorAll('.screen-text');
+const screen = document.querySelector('.screen');
+const operators = document.querySelectorAll('.operator');
+const dotButton = document.querySelector('#dot');
 let operatorCount = 0;
-operators.forEach(operator => operator.addEventListener("mouseup", operateFirstTwoNum));
-buttons.forEach(button => button.addEventListener("mousedown", printScreen));
-document.querySelector(".clear").addEventListener("mousedown", resetValues )
-document.querySelector(".equals-to").addEventListener("mousedown", () => {
-    let calculateString = screen.textContent.split(" ");
+operators.forEach(operator => operator.addEventListener('mouseup', operateFirstTwoNum));
+buttons.forEach(button => button.addEventListener('mousedown', printScreen));
+document.querySelector('.clear').addEventListener('mousedown', resetValues )
+document.querySelector('.equals-to').addEventListener('mousedown', () => {
+    let calculateString = screen.textContent.split(' ');
     operate(...calculateString);
 });
