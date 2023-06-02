62e614713814d37678d6a6c00dddc9cde1524343
Vatsal <rajvatsal2004@gmail.com>
2023-06-02 13:32:56 +0530

    fix: No consecutive operators anymore

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
 function operate(n1, op, n2){
     n1 = Math.floor(parseFloat(n1) * 100) / 100;
     n2 = Math.floor(parseFloat(n2) * 100) / 100;
-    Number.isNaN(n2)? n2 = n1: n2 = n2;
     this.calc = {
         "+":  (a, b) => a + b,
         "-":  (a, b) => a - b,
         "*":  (a, b) => Math.floor((a * b) * 100) / 100,
         "/":  (a, b) => {
             if(b === 0) getRickRolled();
-            Math.floor((a / b) * 100) / 100;
+            else Math.floor((a / b) * 100) / 100;
         },
         "**": (a, b) => a ** b,
         "%":  (a, b) => a % b,
     }
     screen.textContent = this.calc[op](n1, n2)
 }
-function printScreen(e){
-    if(e.target.textContent === ".") e.target.removeEventListener('mousedown', printScreen);
+function printNumber(e){
+    if(e.target.textContent === ".") e.target.removeEventListener('mousedown', printNumber);
+    operators.forEach(operator => operator.addEventListener('mousedown', operateFirstTwoNum));
+    operators.forEach(operator => operator.addEventListener('mousedown', printOperators));
+    screen.textContent += e.target.textContent;
+}
+function printOperators(e){
+    dotButton.addEventListener('mousedown', printNumber);
+    operators.forEach(operator => operator.removeEventListener('mousedown', printOperators));
     screen.textContent += e.target.textContent;
 }
 function operateFirstTwoNum(e){
     operatorCount++;
-    dotButton.addEventListener('mousedown', printScreen);
-
+    operators.forEach(operator => operator.removeEventListener('mousedown', operateFirstTwoNum));
     if(operatorCount !== 2) return
+    operators.forEach(operator => operator.addEventListener('mousedown', operateFirstTwoNum));
     let calculateString = screen.textContent.split(" ");
     operate(...calculateString);
     if(screen.textContent === "") {
         resetValues();
         return
     }
-    screen.textContent += e.target.textContent;
     operatorCount--;
 }
 function resetValues(){
     operatorCount = 0;
-    dotButton.addEventListener('mousedown', printScreen);
+    operators.forEach(operator => operator.addEventListener('mouseup', operateFirstTwoNum));
+    operators.forEach(operator => operator.addEventListener('mousedown', printOperators));
+    numbers.forEach(number => number.addEventListener('mousedown', printNumber));
+    dotButton.addEventListener('mousedown', printNumber);
     screen.textContent = "";
 }
 function getRickRolled(){
@@ -46,18 +54,19 @@ function getRickRolled(){
     vid.setAttribute('width', '100%');
     src.setAttribute('src', './pics-vids/get-rick-rolled.mp4');
     vid.appendChild(src);
-    main.insertBefore(vid, screen);
-    vid.play();
-    vid.addEventListener('ended', () => main.removeChild(vid));
+    //main.insertBefore(vid, screen);
+    //vid.play();
+    //vid.addEventListener('ended', () => main.removeChild(vid));
 }
 
-const buttons = document.querySelectorAll('.screen-text');
+const numbers = document.querySelectorAll('.number');
 const screen = document.querySelector('.screen');
 const operators = document.querySelectorAll('.operator');
 const dotButton = document.querySelector('#dot');
 let operatorCount = 0;
-operators.forEach(operator => operator.addEventListener('mouseup', operateFirstTwoNum));
-buttons.forEach(button => button.addEventListener('mousedown', printScreen));
+operators.forEach(operator => operator.addEventListener('mousedown', operateFirstTwoNum));
+operators.forEach(operator => operator.addEventListener('mousedown', printOperators));
+numbers.forEach(number => number.addEventListener('mousedown', printNumber));
 document.querySelector('.clear').addEventListener('mousedown', resetValues )
 document.querySelector('.equals-to').addEventListener('mousedown', () => {
     let calculateString = screen.textContent.split(' ');
