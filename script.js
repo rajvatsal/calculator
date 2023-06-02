d45083c9e1cc52824311548688fcb17c4e98259d
Vatsal <rajvatsal2004@gmail.com>
2023-06-02 08:14:23 +0530

    fix: removeEventListener from dot if a num has one

--- script.js
+++ script.js
@@ -13,10 +13,12 @@ function operate(n1, op, n2){
     screen.textContent = this.calc[op](n1, n2)
 }
 function printScreen(e){
+    if(e.target.textContent === ".") e.target.removeEventListener("mousedown", printScreen);
     screen.textContent += e.target.textContent;
 }
 function operateFirstTwoNum(e){
     operatorCount++;
+    dot.addEventListener("mousedown", printScreen);
     if(operatorCount !== 2) return;
     let calculateString = screen.textContent.split(" ");
     operate(...calculateString);
@@ -25,12 +27,13 @@ function operateFirstTwoNum(e){
 }
 function resetValues(e){
     operatorCount = 0;
-    screen.textContent = ""
+    screen.textContent = "";
 }
 
 const buttons = document.querySelectorAll(".screen-text");
 const screen = document.querySelector(".screen");
 const operators = document.querySelectorAll(".operator");
+const dotButton = document.querySelector("#dot");
 let operatorCount = 0;
 operators.forEach(operator => operator.addEventListener("mouseup", operateFirstTwoNum));
 buttons.forEach(button => button.addEventListener("mousedown", printScreen));
